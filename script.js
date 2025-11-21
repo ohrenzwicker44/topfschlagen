dc73afb5b65899827842dcd0c3eeeffb0361384f
ohrenzwicker44 <finni.sch@gmx.de>
2025-11-22 00:18:11 +0100

    Update HTML und JS

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function getLocation() {
-    navigator.geolocation.getCurrentPosition(position => {
-        console.log("Breitengrad:", position.coords.latitude);
-        console.log("Längengrad:", position.coords.longitude);
-    });
+    // Prüfen, ob der Browser Geolocation unterstützt
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                // Alert zeigt die Daten an
+                alert(
+                    "Breitengrad: " + position.coords.latitude + "\n" +
+                    "Längengrad: " + position.coords.longitude
+                );
+            },
+            function(error) {
+                // Fehlerbehandlung, falls der Nutzer ablehnt oder etwas schiefläuft
+                alert("Fehler beim Abrufen der Position: " + error.message);
+            }
+        );
+    } else {
+        alert("Geolocation wird von deinem Browser nicht unterstützt.");
+    }
 }
